325640de7e86e0b87a77c3fe4964408ea15fb69d
Jose Bogarin Solano <jbogarin@altus.cr>
2020-10-31 21:58:15 -0600

    Included devices and topology examples

---
+++ examples/devices/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+
+	dnac "github.com/cisco-en-programmability/dnacenter-go-sdk/sdk"
+)
+
+// Client is DNA Center API client
+var Client *dnac.Client
+
+func main() {
+	fmt.Println("Authenticating")
+	Client = dnac.NewClientWithOptions("https://sandboxdnac.cisco.com",
+		"devnetuser", "Cisco123!",
+		"false", "false")
+	fmt.Println("Getting device count")
+	devicesCount, _, err := Client.Devices.GetDeviceCount()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(devicesCount.Response)
+
+	fmt.Println("Printing device list ...")
+
+	getDeviceListQueryParams := &dnac.GetDeviceListQueryParams{}
+	devices, _, err := Client.Devices.GetDeviceList(getDeviceListQueryParams)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for id, device := range devices.Response {
+		fmt.Println("GET:", id, device.ID, device.MacAddress, device.ManagementIPAddress, device.PlatformID)
+	}
+
+	getDeviceListQueryParams = &dnac.GetDeviceListQueryParams{
+		PlatformID: []string{"C9300-24UX"},
+	}
+
+	fmt.Println("Printing device list  ... PlatformID is C9300-24UX")
+	devices, _, err = Client.Devices.GetDeviceList(getDeviceListQueryParams)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for id, device := range devices.Response {
+		fmt.Println("GET:", id, device.ID, device.MacAddress, device.ManagementIPAddress, device.PlatformID)
+	}
+
+	fmt.Println("Printing device info by device id...")
+	device, _, err := Client.Devices.GetDeviceByID(devices.Response[0].ID)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(device.Response.ID, device.Response.MacAddress, device.Response.ManagementIPAddress, device.Response.PlatformID)
+
+}
---
+++ examples/topology/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+
+	dnac "github.com/cisco-en-programmability/dnacenter-go-sdk/sdk"
+)
+
+// Client is DNA Center API client
+var Client *dnac.Client
+
+func main() {
+	fmt.Println("Authenticating...")
+	Client = dnac.NewClientWithOptions("https://sandboxdnac.cisco.com",
+		"devnetuser", "Cisco123!",
+		"false", "false")
+
+	fmt.Println("Printing site topology...")
+	topology, _, err := Client.Topology.GetSiteTopology()
+	if err != nil {
+		fmt.Println(err)
+	}
+	for id, site := range topology.Response.Sites {
+		fmt.Println("GET:", id, site.ID, site.GroupNameHierarchy)
+	}
+
+	fmt.Println("Printing physical topology...")
+
+	getPhysicalTopologyQueryParams := &dnac.GetPhysicalTopologyQueryParams{
+		NodeType: "",
+	}
+	physicalTopology, _, err := Client.Topology.GetPhysicalTopology(getPhysicalTopologyQueryParams)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for id, nodes := range physicalTopology.Response.Nodes {
+		fmt.Println("GET:", id, nodes.ID, nodes.IP, nodes.Label, nodes.AdditionalInfo)
+	}
+
+	fmt.Println("Printing VLAN Information...")
+	vlanInformation, _, err := Client.Topology.GetVLANDetails()
+	if err != nil {
+		fmt.Println(err)
+	}
+	for id, name := range vlanInformation.Response {
+		fmt.Println("GET:", id, name)
+	}
+
+	fmt.Println("Printing VLAN 1 Details ...")
+	vlanDetails, _, err := Client.Topology.GetTopologyDetails("1")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for id, link := range vlanDetails.Response.Links {
+		fmt.Println("GET:", id, link.ID, link.Source, link.Target, link.LinkStatus)
+	}
+
+	fmt.Println("Printing Network Health...")
+	getOverallNetworkHealthQueryParams := &dnac.GetOverallNetworkHealthQueryParams{
+		Timestamp: "",
+	}
+	networkHealth, _, err := Client.Topology.GetOverallNetworkHealth(getOverallNetworkHealthQueryParams)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(networkHealth.HealthDistirubution, networkHealth.LatestHealthScore, networkHealth.Response, networkHealth.MonitoredDevices)
+}
