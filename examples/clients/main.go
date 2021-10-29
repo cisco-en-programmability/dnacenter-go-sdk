fd2660911e20aee19ddf5e836f0cc3626c499045
William Astorga <wastorga@altus.co.cr>
2021-10-29 10:01:40 -0600

    Add headers for GetClientEnrichmentDetails #3

---
+++ examples/clients/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	dnac "dnacenter-go-sdk/sdk"
+)
+
+// Client is DNA Center API client
+var client *dnac.Client
+
+func main() {
+	var err error
+	fmt.Println("Authenticating")
+	client, err = dnac.NewClientWithOptions("https://192.168.196.2/",
+		"altus", "Altus123",
+		"true", "false")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	getClientEnrichmentDetailsHeaderParams := &dnac.GetClientEnrichmentDetailsHeaderParams{
+		EntityType:    "network_user_id",
+		EntityValue:   "test",
+		IssueCategory: "test",
+	}
+	nResponse, _, err := client.Clients.GetClientEnrichmentDetails(getClientEnrichmentDetailsHeaderParams)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(nResponse)
+
+}
