b12c980e4be28524f2447dee5beaf52775b77f71
fmunoz <fmunoz@altus.cr>
2023-07-12 10:50:29 -0600

    ## [5.0.7] - 2023-07-12 ### Changed - `RequestDeviceOnboardingPnpClaimADeviceToASiteConfigInfo` struct is now a pointer. - `ConfigParameters` struct is now a pointer. - `Skip` is a pointer.

--- sdk/device_onboarding_pnp.go
+++ sdk/device_onboarding_pnp.go
@@ -3285,26 +3285,26 @@ type RequestDeviceOnboardingPnpResetDeviceDeviceResetListConfigListConfigParamet
 	Value string `json:"value,omitempty"` //
 }
 type RequestDeviceOnboardingPnpClaimADeviceToASite struct {
-	DeviceID      string                                                  `json:"deviceId,omitempty"`      // Device Id
-	SiteID        string                                                  `json:"siteId,omitempty"`        // Site Id
-	Type          string                                                  `json:"type,omitempty"`          // Type
-	ImageInfo     RequestDeviceOnboardingPnpClaimADeviceToASiteImageInfo  `json:"imageInfo"`               //
-	ConfigInfo    RequestDeviceOnboardingPnpClaimADeviceToASiteConfigInfo `json:"configInfo"`              //
-	RfProfile     string                                                  `json:"rfProfile,omitempty"`     // for Access Points
-	StaticIP      string                                                  `json:"staticIP,omitempty"`      // for CatalystWLC/MobilityExpress
-	SubnetMask    string                                                  `json:"subnetMask,omitempty"`    // for CatalystWLC/MobilityExpress
-	Gateway       string                                                  `json:"gateway,omitempty"`       // for CatalystWLC/MobilityExpress
-	VLANID        string                                                  `json:"vlanID,omitempty"`        // for Catalyst 9800 WLC
-	InterfaceName string                                                  `json:"interfaceName,omitempty"` // for Catalyst 9800 WLC
-	SensorProfile string                                                  `json:"sensorProfile,omitempty"` // for Sensors
+	DeviceID      string                                                   `json:"deviceId,omitempty"`      // Device Id
+	SiteID        string                                                   `json:"siteId,omitempty"`        // Site Id
+	Type          string                                                   `json:"type,omitempty"`          // Type
+	ImageInfo     *RequestDeviceOnboardingPnpClaimADeviceToASiteImageInfo  `json:"imageInfo"`               //
+	ConfigInfo    *RequestDeviceOnboardingPnpClaimADeviceToASiteConfigInfo `json:"configInfo"`              //
+	RfProfile     string                                                   `json:"rfProfile,omitempty"`     // for Access Points
+	StaticIP      string                                                   `json:"staticIP,omitempty"`      // for CatalystWLC/MobilityExpress
+	SubnetMask    string                                                   `json:"subnetMask,omitempty"`    // for CatalystWLC/MobilityExpress
+	Gateway       string                                                   `json:"gateway,omitempty"`       // for CatalystWLC/MobilityExpress
+	VLANID        string                                                   `json:"vlanID,omitempty"`        // for Catalyst 9800 WLC
+	InterfaceName string                                                   `json:"interfaceName,omitempty"` // for Catalyst 9800 WLC
+	SensorProfile string                                                   `json:"sensorProfile,omitempty"` // for Sensors
 }
 type RequestDeviceOnboardingPnpClaimADeviceToASiteImageInfo struct {
 	ImageID string `json:"imageId"` // Image Id
-	Skip    bool   `json:"skip"`    // Skip
+	Skip    *bool  `json:"skip"`    // Skip
 }
 type RequestDeviceOnboardingPnpClaimADeviceToASiteConfigInfo struct {
-	ConfigID         string                                                                    `json:"configId"`         // Config Id
-	ConfigParameters []RequestDeviceOnboardingPnpClaimADeviceToASiteConfigInfoConfigParameters `json:"configParameters"` //
+	ConfigID         string                                                                     `json:"configId"`         // Config Id
+	ConfigParameters *[]RequestDeviceOnboardingPnpClaimADeviceToASiteConfigInfoConfigParameters `json:"configParameters"` //
 }
 type RequestDeviceOnboardingPnpClaimADeviceToASiteConfigInfoConfigParameters struct {
 	Key   string `json:"key"`   // Key
